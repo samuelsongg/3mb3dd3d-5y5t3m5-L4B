2837daca57da50d29491d516562e5520c7a65dd6
Samuel Song <45842379+samuelsongg@users.noreply.github.com>
2023-10-13 21:20:50 +0800

    Update 2200915.c

--- Lab 6/2200915.c
+++ Lab 6/2200915.c
@@ -1,55 +1,50 @@
 #include <stdio.h> 
-#include <unistd.h>
-#include "pico/stdlib.h" 
+#include "pico/stdlib.h" // For sleep_ms()
+ 
+float Kp = 2.0; 
+float Ki = 0.2; 
+float Kd = 0.02; 
+ 
+// Function to compute the control signal 
+float compute_pid(float setpoint, float current_value, float *integral, float *prev_error) { 
+    float error = setpoint - current_value;  // Reversed the error calculation 
+ 
+    *integral += error; 
+ 
+    float derivative = error - *prev_error; 
+ 
+    float control_signal = Kp * error + Ki * (*integral) + Kd * derivative; 
+ 
+    *prev_error = error;  // Set previous error to current error 
+ 
+    return control_signal; 
+} 
  
 int main() { 
     stdio_init_all();
 
-    // Initialize PID controller parameters 
-    double Kp = 1.0; 
-    double Ki = 0.1; 
-    double Kd = 0.01; 
+    float setpoint = 100.0;  // Desired position 
+    float current_value = 0.0;  // Current position 
+    float integral = 0.0;  // Integral term 
+    float prev_error = 0.0;  // Previous error term 
  
-    // Initialize variables 
-    double setpoint = 100.0; 
-    double current_value = 0.0; 
-    double integral = 0.0; 
-    double prev_error = 0.0; 
+    float time_step = 0.1; 
+    int num_iterations = 100;  // Add a missing semicolon 
  
-    // Initialize simulation parameters 
-    float time_step = 100; 
-    int num_iterations = 100; 
- 
-    // Main control loop 
+    // Simulate the control loop 
     for (int i = 0; i < num_iterations; i++) { 
-        // Compute error 
-        double error = setpoint - current_value; 
+        float control_signal = compute_pid(setpoint, current_value, &integral, &prev_error);  // Pass the address of prev_error 
  
-        // Update integral term 
-        integral += error; 
+        float motor_response = control_signal * 0.05;  // Motor response model 
  
-        // Compute derivative term 
-        double derivative = error - prev_error; 
+        current_value += motor_response;  // Update the current value 
  
-        // Compute control signal 
-        double control_signal = Kp * error + Ki * integral + Kd * derivative; 
- 
-        // Update previous error 
-        prev_error = error; 
- 
-        // Simulate motor dynamics (for demonstration purposes) 
-        double motor_response = control_signal * 0.1; 
- 
-        // Update current position 
-        current_value += motor_response; 
- 
-        // Display results 
         printf("Iteration %d: Control Signal = %f, Current Position = %f\n", i, control_signal, current_value); 
  
-        // Sleep for the time step (for demonstration purposes) 
-        sleep_ms(time_step); 
+        sleep_ms(time_step * 1000);  // Convert to milliseconds
     } 
-    // End of main control loop 
-} 
  
+    return 0; 
+}
+
 /*** end of file ***/
