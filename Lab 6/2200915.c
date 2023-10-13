8c91defa7a973694910baba550c809fa8c592892
Samuel Song <45842379+samuelsongg@users.noreply.github.com>
2023-10-13 21:04:31 +0800

    Update 2200915.c

--- Lab 6/2200915.c
+++ Lab 6/2200915.c
@@ -1,52 +1,55 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <unistd.h>
+#include "pico/stdlib.h" 
+ 
+int main() { 
+    stdio_init_all();
 
-int main() {
-    // Initialize PID controller parameters
-    double Kp = 1.0;
-    double Ki = 0.1;
-    double Kd = 0.01;
-
-    // Initialize variables
-    double setpoint = 100.0;
-    double current_value = 0.0;
-    double integral = 0.0;
-    double prev_error = 0.0;
-
-    // Initialize simulation parameters
-    double time_step = 0.1;
-    int num_iterations = 100;
-
-    // Main control loop
-    for (int i = 0; i < num_iterations; i++) {
-        // Compute error
-        double error = setpoint - current_value;
-
-        // Update integral term
-        integral += error;
-
-        // Compute derivative term
-        double derivative = error - prev_error;
-
-        // Compute control signal
-        double control_signal = Kp * error + Ki * integral + Kd * derivative;
-
-        // Update previous error
-        prev_error = error;
-
-        // Simulate motor dynamics (for demonstration purposes)
-        double motor_response = control_signal * 0.1;
-
-        // Update current position
-        current_value += motor_response;
-
-        // Display results
-        printf("Iteration %d: Control Signal = %f, Current Position = %f\n", i, control_signal, current_value);
-
-        // Sleep for the time step (for demonstration purposes)
-        sleep(time_step);
-    }
-    // End of main control loop
-}
-
+    // Initialize PID controller parameters 
+    double Kp = 1.0; 
+    double Ki = 0.1; 
+    double Kd = 0.01; 
+ 
+    // Initialize variables 
+    double setpoint = 100.0; 
+    double current_value = 0.0; 
+    double integral = 0.0; 
+    double prev_error = 0.0; 
+ 
+    // Initialize simulation parameters 
+    float time_step = 100; 
+    int num_iterations = 100; 
+ 
+    // Main control loop 
+    for (int i = 0; i < num_iterations; i++) { 
+        // Compute error 
+        double error = setpoint - current_value; 
+ 
+        // Update integral term 
+        integral += error; 
+ 
+        // Compute derivative term 
+        double derivative = error - prev_error; 
+ 
+        // Compute control signal 
+        double control_signal = Kp * error + Ki * integral + Kd * derivative; 
+ 
+        // Update previous error 
+        prev_error = error; 
+ 
+        // Simulate motor dynamics (for demonstration purposes) 
+        double motor_response = control_signal * 0.1; 
+ 
+        // Update current position 
+        current_value += motor_response; 
+ 
+        // Display results 
+        printf("Iteration %d: Control Signal = %f, Current Position = %f\n", i, control_signal, current_value); 
+ 
+        // Sleep for the time step (for demonstration purposes) 
+        sleep_ms(time_step); 
+    } 
+    // End of main control loop 
+} 
+ 
 /*** end of file ***/
